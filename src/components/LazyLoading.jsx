fcd8b5a052cc0b17389da7fd6ca117479c5d230c
Rajath Shetty <grash2024@gmail.com>
2024-12-03 19:04:50 +0530

    Component LazyLoading renamed to Body

--- src/components/LazyLoading.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const LazyLoading = () => {
-	return <div>LazyLoading</div>;
-};
-
-export default LazyLoading;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Main from "./components/Main.jsx";
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <Main />,
+	},
+]);
+createRoot(document.getElementById("root")).render(
+	<StrictMode>
+		<RouterProvider router={router} />
+	</StrictMode>
+);
